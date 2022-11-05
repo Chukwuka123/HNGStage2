deab2736b00a88c555435a97eee3d7087753fee0
MAC <mac@MACs-MacBook-Pro.local>
2022-11-05 18:32:05 +0100

    still the hng stage 2 task,added vercel.json to it

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function Home() {
         <img id="profile_img" src="https://pbs.twimg.com/profile_images/1516046767678271496/xB1dNgVB_400x400.jpg" alt="image.jpeg" className=" items-center h-[92px] w-[92px] bg-gray-200 rounded-full justify-center" />
       </div>
       <div className="max-w-[800px] flex justify-center items-center font-black">
-        <h1 >Chukky</h1>
+        <h1 id="slack" >Chukky</h1>
       </div>
       <div className=" max-w-[800px]">
         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4"  id="twitter"  href="https://twitter.com/Chukky84573297" target="_blank">Twitter Link</a>
@@ -18,12 +18,12 @@ function Home() {
         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4" id="book_python" ref_id="Chukwuka Nonyelim" href="https://books.zuri.team/" target="_blank">Python Books</a>
         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4" id="pitch" href="https://background.zuri.team" target="_blank">Background Check for Coders</a>
         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4" id="book_design" href="https://books.zuri.team/design-rules" target="_blank">Design Books</a>
-         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4" href="/contact">Contact Me</a>
+         <a className="flex justify-center items-center w-full h-[40px] bg-gray-200 rounded-md mb-4" id="contact" href="/contact">Contact Me</a>
         < div className="flex justify-center items-center">
             <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github logo" width="10%" height="50px"/>
             <img  src="https://launchpad.syr.edu/wp-content/uploads/2021/09/Slack-logo.jpg" alt="slack logo" width="10%" height="50px"/>
     </div>
-    <div className="flex text-center w-full justify-center items-center max-w-[800px]">
+    <div className="justify-center space-x-25 m-auto flex w-full">
 <p className="font-black">Zuri.Internship</p> 
 
  <p>HNG Internship 9 Frontend Task</p> 
@@ -31,7 +31,7 @@ function Home() {
   <img src="https://i.ytimg.com/vi/OR05gnlvTHY/maxresdefault.jpg" alt="I4G logo" width="10%" height="50px"/>
     </div>
       </div >
-      <Link className="font-black flex justify-center max-w-[800px]" to="/contact">Contact Page</Link>
+      <Link className="font-black" to="/contact">CONTACT PAGE</Link>
 
     </>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,15 +7,25 @@ function Contact() {
         <div className="max-w-[800px]" >
           <h2 className="text-3xl">Contact Me</h2>
           <p>Hi there . Contact me to ask me about anything you have in mind.</p>
-
-          <label className="flex" id="First Name">First Name <input className="rounded-md border block py-2 px-3 text-grey-darkest" placeholder="Enter your first name" required/></label>
-          <label className="flex" id="Last Name">Last Name <input className="rounded-md border block" placeholder="Enter your last name"/></label>
-           <label> Email <input id="Email" className="justify-between rounded-md border w-full block" placeholder="yourname@email.com "/></label>
+           <div className="flex gap-x-8">
+          <label id="First Name">First Name <br/><input className="rounded-md border block py-2 px-3 text-grey-darkest" placeholder="Enter your first name" required/></label>
+          <label className="place-content-center items-start" id="Last Name">Last Name <br /><input className=" rounded-md border block py-2 px-3 text-grey-darkest justify-center" placeholder="Enter your last name"/></label>
+           </div>
+           <label> Email <input id="Email" className="justify-between rounded-md border py-2 px-3 w-full block" placeholder="yourname@email.com "/></label>
           <label >Message <textarea rows="5"  id="message" className="rounded-md border w-full block" placeholder="send me a message and i'll reply you as soon as possible"></textarea></label>
           <label > <input type="checkbox"/> You agree to providing valid data to who may contact you</label>
           <button className="max-w-[800px] w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded block" id="btn_submit">Send Message</button>
-        </div>
-        <nav>
+        
+       
+        <div className="justify-center space-x-20 m-auto flex w-full">
+<p className="font-black">Zuri.Internship</p> 
+
+ <p>HNG Internship 9 Frontend Task</p> 
+ 
+  <img src="https://i.ytimg.com/vi/OR05gnlvTHY/maxresdefault.jpg" alt="I4G logo" width="10%" height="50px"/>
+    </div>
+    </div>
+    <nav>
           <Link className="font-black" to="/">HOMEPAGE</Link>
         </nav>
       </>
